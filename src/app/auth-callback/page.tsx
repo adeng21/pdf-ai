3e0adf878cf5748eb8ba0f834bb41769e03f42ec
Albert Deng <adeng21@gmail.com>
2023-10-15 15:46:05 -0700

    setup auth callback route

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,14 +1,21 @@
 import { useSearchParams } from "next/navigation"
 import { useRouter } from "next/router"
+import { trpc } from "../_trpc/client"
 
-const Page = () => {{
+const Page = async () => {
     const router = useRouter()
     const searchParams = useSearchParams()
     const origin = searchParams.get("origin")
 
+    const {data, isLoading} = trpc.authCallback.useQuery(undefined, {
+        onSuccess: ({success}) => {
+            if(success) {
+                // user is synced to db
+                router.push(origin ? `/${origin}` : `/dashboard`)
+            }
+        }
+    }
     
-    
-
     
 }
 export default Page
