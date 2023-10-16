2b141bf3ac728c9f970ca6b00521c435d6c611c1
Albert Deng <adeng21@gmail.com>
2023-10-16 15:59:41 -0700

    progress bar pretty

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog";
 import { Button } from "./ui/button";
 import Dropzone from "react-dropzone";
-import { Cloud, File } from "lucide-react";
+import { Cloud, File, Loader2 } from "lucide-react";
 import { Progress } from "./ui/progress";
 import { useUploadThing } from "@/lib/uploadthing";
 import { useToast } from "./ui/use-toast";
@@ -105,7 +105,16 @@ const UploadDropzone = () => {
                   <Progress
                     value={uploadProgress}
                     className="h-1 w-full bg-zinc-200"
+                    indicatorColor={
+                      uploadProgress === 100 ? "bg-green-500" : ""
+                    }
                   />
+                  {uploadProgress === 100 ? (
+                    <div className="flex gap-1 items-center justify-center text-sm text-zinc-700 text-center py-2">
+                      <Loader2 className="h-3 w-3 animate-spin" />
+                      Redirecting...
+                    </div>
+                  ) : null}
                 </div>
               ) : null}
               <input
