c2e60178958473f677a24c4268348f01c4735a3e
Albert Deng <adeng21@gmail.com>
2023-10-16 14:37:26 -0700

    upload file modal drag and drop and progress bar

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -1,4 +1,10 @@
 const PdfRenderer = () => {
-  return <div>PDF Renderer</div>;
+  return (
+    <div className="w-full rounded-m bg-white shadow flex flex-col items-center">
+      <div className="h-14 border- w-full border-zin-200 flex items-center justify-between px-2">
+        <div className="flex items-center gap-1.5">top bar</div>
+      </div>
+    </div>
+  );
 };
 export default PdfRenderer;
