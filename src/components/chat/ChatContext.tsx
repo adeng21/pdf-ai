9a9bd3be3ee0d74a9c6f8a64b7a6115da16f5796
Albert Deng <adeng21@gmail.com>
2023-10-18 15:50:32 -0700

    optimistically update message

--- src/components/chat/ChatContext.tsx
+++ src/components/chat/ChatContext.tsx
@@ -1,7 +1,9 @@
-import { createContext } from "react";
+import { createContext, useRef } from "react";
 import { useState } from "react";
 import { useToast } from "../ui/use-toast";
 import { useMutation } from "@tanstack/react-query";
+import { trpc } from "@/app/_trpc/client";
+import { INFINITE_QUERY_LIMIT } from "@/config/infinite-query";
 
 type StreamResponse = {
   addMessage: () => void;
@@ -25,6 +27,8 @@ export const ChatContextProvider = ({ fileId, children }: Props) => {
   const [message, setMessage] = useState<string>("");
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const { toast } = useToast();
+  const utils = trpc.useContext();
+  const backupMessage = useRef("");
 
   //not using TRPC, using react query - streaming response - trpc only supports json
   const { mutate: sendMessage } = useMutation({
@@ -37,6 +41,67 @@ export const ChatContextProvider = ({ fileId, children }: Props) => {
         throw new Error("failed to send message");
       }
       return response.body;
+    },
+    onMutate: async ({ message }) => {
+      // # save message to ref
+      backupMessage.current = message;
+      // clear input
+      setMessage("");
+
+      // step 1 - cancel inflight requests
+      await utils.getFileMessages.cancel();
+
+      // step 2 - get past messages
+      const previousMessages = utils.getFileMessages.getInfiniteData();
+
+      // step 3 - optimistically insert new message
+      utils.getFileMessages.setInfiniteData(
+        { fileId, limit: INFINITE_QUERY_LIMIT },
+        (old) => {
+          if (!old) {
+            return {
+              pages: [],
+              pageParams: [],
+            };
+          }
+          let newPages = [...old.pages];
+          let latestPage = newPages[0]!;
+          latestPage.messages = [
+            {
+              createdAt: new Date().toISOString(),
+              id: crypto.randomUUID(),
+              text: message,
+              isUserMessage: true,
+            },
+            ...latestPage.messages,
+          ];
+
+          newPages[0] = latestPage;
+
+          return {
+            ...old,
+            pages: newPages,
+          };
+        }
+      );
+      // step 4 - add loading state after message is already displayed
+      setIsLoading(true);
+      return {
+        previousMessages:
+          previousMessages?.pages.flatMap((page) => page.messages) ?? [],
+      };
+    },
+    onError: (_, __, context) => {
+      setMessage(backupMessage.current);
+      utils.getFileMessages.setData(
+        { fileId },
+        { messages: context?.previousMessages ?? [] }
+      );
+    },
+    onSettled: async () => {
+      setIsLoading(false);
+      // refresh entire data
+      await utils.getFileMessages.invalidate({ fileId });
     },
   });
   const addMessage = async () => sendMessage({ message });
