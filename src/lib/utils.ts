ae6981887ceb84e322c23efeb24daf43729d090d
Albert Deng <adeng21@gmail.com>
2023-10-20 09:36:06 -0700

    update metadata

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,5 @@
 import { type ClassValue, clsx } from "clsx"
+import { Metadata } from "next"
 import { twMerge } from "tailwind-merge"
  
 export function cn(...inputs: ClassValue[]) {
@@ -11,4 +12,31 @@ export function absoluteUrl(path: string) {
     return `https://${process.env.VERCEL_URL}${path}`
   }
   return `https://localhost:${process.env.PORT ?? 3000}${path}`
+}
+export function constructMetadata({
+  title = 'PDF AI',
+  description = 'Make chatting to your PDF files easy',
+  image = "/thumbnail.png",
+  icons = "/favicon.ico",
+  noIndex = false,
+}: {
+  title?: string,
+  description?: string,
+  image?: string,
+  icons?: string,
+  noIndex?: boolean,
+} = {}): Metadata{
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [{url: image}]
+    },
+    icons,
+    metadataBase: new URL('https://pdf-ai-alpha.vercel.app'),
+    themeColor: "#FFF",
+    ...(noIndex && { robots: { index: false, follow: false }})
+  }
 }
